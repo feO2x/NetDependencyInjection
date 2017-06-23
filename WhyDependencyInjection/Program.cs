e9797099ec39a37531bf991cf835be4e0170fca1
Kenny Pflug <kenny.pflug@live.de>
2017-06-23 14:31:27 +0200

    added project for Copy example

---
+++ WhyDependencyInjection/Program.cs
@@ -0,0 +1,16 @@
+﻿
+namespace WhyDependencyInjection
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+            Copy();
+        }
+
+        private static void Copy()
+        {
+            
+        }
+    }
+}
