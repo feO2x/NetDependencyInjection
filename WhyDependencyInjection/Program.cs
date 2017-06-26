f5164db7f46624e4ed3595db8ae45e78aba4ed28
Kenny Pflug <kenny.pflug@live.de>
2017-06-26 18:33:11 +0200

    added LightInject to resolve object graphs

--- WhyDependencyInjection/Program.cs
+++ WhyDependencyInjection/Program.cs
@@ -1,4 +1,6 @@
 ﻿
+using LightInject;
+
 namespace WhyDependencyInjection
 {
     public static class Program
@@ -6,16 +8,26 @@ namespace WhyDependencyInjection
         public static void Main()
         {
             // Register
-            var reader = new ConsoleReader();
-            //var writer = new ConsoleWriter();
-            var writer = new FileWriter("text.txt");
-            var copyProcess = new CopyProcess(reader, writer);
+            var container = new ServiceContainer();
+            container.Register<IReader, ConsoleReader>()
+                     //.Register<IWriter, ConsoleWriter>()
+                     .Register<IWriter>(f => new FileWriter("text.txt"), new PerRequestLifeTime())
+                     .Register<CopyProcess>();
 
-            // Resolve
-            copyProcess.Copy();
+
+            //var reader = new ConsoleReader();
+            ////var writer = new ConsoleWriter();
+            //var writer = new FileWriter("text.txt");
+            using (container.BeginScope())
+            {
+                var copyProcess = container.GetInstance<CopyProcess>();
+
+                // Resolve
+                copyProcess.Copy();
+            }
 
             // Release
-            writer.Dispose();
+            //writer.Dispose();
         }
     }
 }
